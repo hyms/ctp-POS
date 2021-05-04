b827d065a5fd1e1aa12c405c9abe73439d04d013
hyms <hdnymib@gmail.com>
2021-05-03 22:20:46 -0400

    order ordenes and add view anulado

--- resources/js/Shared/menu/MainMenu.vue
+++ resources/js/Shared/menu/MainMenu.vue
@@ -2,27 +2,27 @@
     <!--------------------
               START - Main Menu
               -------------------->
-        <Menu/>
+    <Menu/>
 </template>
 
 <script>
 import Menu from "./Menu";
 
 export default {
-        data() {
-            return {
-                images: {
-                    logo: "../img/logo.png",
-                    avatar: "../img/avatar1.jpg"
-                },
-                botonMenu: false,
-            };
-        },
-        components: {
-            Menu
-        },
-        props:{
-            nombre: String,
-        }
-    };
+    data() {
+        return {
+            images: {
+                logo: "../img/logo.png",
+                avatar: "../img/avatar1.jpg"
+            },
+            botonMenu: false,
+        };
+    },
+    components: {
+        Menu
+    },
+    props: {
+        nombre: String,
+    }
+};
 </script>
