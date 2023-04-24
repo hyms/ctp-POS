f2f16f5602635031e892295d0bd0182cca2dce15
Zackks <hdnymib@gmail.com>
2023-04-24 10:47:43 -0400

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <link rel="shortcut icon" href="/favicon.jpeg">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
@@ -12,15 +12,10 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-        @routes
+        @vite('resources/js/app.js')
         @inertiaHead
-        @vite(['resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body>
         @inertia
-
-        @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
-        @endenv
     </body>
 </html>
